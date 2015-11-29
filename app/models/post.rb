b6ec321a9dfd10341c9c219197cf1a8d6634baaa
arycancer <arycancer@gmail.com>
2015-11-29 21:17:44 +0700

    fix bug for resolution upload image

--- app/models/post.rb
+++ app/models/post.rb
@@ -15,18 +15,18 @@ class Post < ActiveRecord::Base
           
   # 640x360
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
-  validate :check_dimensions
+  # validate :check_dimensions
   
-  def check_dimensions
-    temp_file = image.queued_for_write[:original]
-    unless temp_file.nil?
-      dimensions = Paperclip::Geometry.from_file(temp_file)
-      width = dimensions.width
-      height = dimensions.height
-
-      if width < 640 && height < 360
-        errors[''] << " dimensions are too small. For a good quality background please upload a larger image. Minimum width: 640px, minimum height: 360px"
-      end
-    end
-  end
+  # def check_dimensions
+  #   temp_file = image.queued_for_write[:original]
+  #   unless temp_file.nil?
+  #     dimensions = Paperclip::Geometry.from_file(temp_file)
+  #     width = dimensions.width
+  #     height = dimensions.height
+  # 
+  #     if width < 640 && height < 360
+  #       errors[''] << " dimensions are too small. For a good quality background please upload a larger image. Minimum width: 640px, minimum height: 360px"
+  #     end
+  #   end
+  # end
 end
